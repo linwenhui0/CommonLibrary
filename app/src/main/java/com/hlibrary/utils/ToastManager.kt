95fa9059e8cf02de2929dffada7b50264d318860
linwenhui0 <1252908234@qq.com>
2020-01-08 21:36:01 +0800

    no message

---
+++ app/src/main/java/com/hlibrary/utils/ToastManager.kt
@@ -0,0 +1,63 @@
+package com.hlibrary.utils
+
+import android.content.Context
+import android.support.annotation.StringRes
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.widget.Toast
+import com.hlibrary.R
+
+class ToastManager private constructor() {
+
+    companion object {
+        val instance: ToastManager by lazy(mode = LazyThreadSafetyMode.SYNCHRONIZED) {
+            ToastManager()
+        }
+        private var toast: Toast? = null
+
+    }
+
+    @Synchronized
+    fun createView(context: Context) {
+        if (toast == null) {
+            toast = Toast(context)
+            val v = LayoutInflater.from(context).inflate(R.layout.view_toast, null)
+            toast?.view = v
+            toast?.setGravity(Gravity.CENTER, 0, 0)
+        }
+    }
+
+    fun showLongToast(context: Context, text: String) {
+        toast?.cancel()
+        createView(context)
+        toast?.setText(text)
+        toast?.duration = Toast.LENGTH_LONG
+        toast?.show()
+    }
+
+    fun showLongToast(context: Context, @StringRes resId: Int) {
+        toast?.cancel()
+        createView(context)
+        toast?.setText(resId)
+        toast?.duration = Toast.LENGTH_LONG
+        toast?.show()
+    }
+
+    fun showShortToast(context: Context, text: String) {
+        toast?.cancel()
+        createView(context)
+        toast?.setText(text)
+        toast?.duration = Toast.LENGTH_SHORT
+        toast?.show()
+    }
+
+    fun showShortToast(context: Context, @StringRes resId: Int) {
+        toast?.cancel()
+        createView(context)
+        toast?.setText(resId)
+        toast?.duration = Toast.LENGTH_SHORT
+        toast?.show()
+    }
+
+
+}
